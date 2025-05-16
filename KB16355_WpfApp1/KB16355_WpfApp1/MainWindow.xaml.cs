fc1adddaf898dc107712beadd62bce68b53c4f93
norikois <33947244+norikois@users.noreply.github.com>
2025-05-16 15:27:00 +0900

    Initial code commit

---
+++ KB16355_WpfApp1/KB16355_WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Infragistics.Controls.Charts;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KB16355_WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var chart = this.xamDataChart1;
+            var xAxis = this.xAxis;
+            var yAxis = this.yAxis;
+
+            var dataView = chart.DataContext as DataView;
+            if (dataView == null) return;
+
+            var dataTable = dataView.Table;
+            if (dataTable == null) return;
+
+            foreach(DataColumn column in dataTable.Columns)
+            {
+                // 1列目はX軸に表示するラベルなのでスキップします。
+                if (dataTable.Columns.IndexOf(column) == 0) continue;
+
+                chart.Series.Add(new ColumnSeries()
+                {
+                    XAxis = xAxis,
+                    YAxis = yAxis,
+                    ItemsSource = dataView,
+                    ValueMemberPath = column.ColumnName,
+                    Title = column.ColumnName,
+                });
+            }
+        }
+    }
+}
---
+++ KB16355_WpfApp1/KB16355_WpfApp1/MainWindowViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KB16355_WpfApp1;
+internal class MainWindowViewModel
+{
+    private DataTable _energyProductionTable;
+
+    public DataView? EnergyProductionView
+    {
+        get => _energyProductionTable?.DefaultView;
+    }
+
+    public MainWindowViewModel()
+    {
+        _energyProductionTable = new ();
+
+        _energyProductionTable.Columns.Add("Country");  // X軸に表示するラベル
+        _energyProductionTable.Columns.Add("Coal", typeof(double));   // 値1つめ
+        _energyProductionTable.Columns.Add("Gas", typeof(double));    // 値2つめ
+        _energyProductionTable.Columns.Add("Hydro", typeof(double));  // 値3つめ
+
+        _energyProductionTable.Rows.Add([ "Country1", 86.4505134, 103.2891932, 126.0406841 ]);
+        _energyProductionTable.Rows.Add([ "Country2", 114.0853903, 82.48463306, 125.9912834 ]);
+        _energyProductionTable.Rows.Add([ "Country3", 127.4458425, 97.7079237, 123.3935982 ]);
+        _energyProductionTable.Rows.Add([ "Country4", 102.5485605, 86.98280705, 114.6298345 ]);
+    }
+}
